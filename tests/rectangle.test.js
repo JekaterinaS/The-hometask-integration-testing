da651834f165a452c02d9b341ff01444e8246a79
ekate <ekaterina89@inbox.lv>
2022-11-09 21:42:12 +0200

    getRectangleArea test added

--- tests/rectangle.test.js
+++ tests/rectangle.test.js
@@ -1,7 +1,13 @@
-import {getRectanglePerimeter} from "../js/rectangle.js";
+import {getRectangleArea, getRectanglePerimeter} from "../js/rectangle.js";
 
 
 test('should properly output rectangle perimeter', () => {
     const rectanglePerimeter = getRectanglePerimeter(9, 4);
     expect(rectanglePerimeter).toBe(26);
+});
+
+test('should properly output rectangle area', () => {
+    const rectangleArea = getRectangleArea(9, 4);
+    expect(rectangleArea).toBe(36);
+
 });
