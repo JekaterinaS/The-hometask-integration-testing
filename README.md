806538ebdd89b01e52be1e08fd29c3f7becdcfa0
ekate <ekaterina89@inbox.lv>
2022-11-10 21:10:09 +0200

    getRectangleInfo test added

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# The-hometask-integration-testing
-With this task you will go through the process of a project creation, setting up the basic structure for writing both code and tests
+# Integration testing
+
+---
+
+### Hometask
+
+With this task bootcampers will go through the process of a project creation, setting up the basic structure for writing both code and tests
+
+---
+
+# Setup
+
+- Run yarn install to install all dependencies.
+- Run yarn test to run your tests.
+- The console output will show you the result
+
+
+
+# Tests
+
+Run nmp install to install all dependencies. You can debug your solution using the comand "npm run debug". The console output will show you the result.
+
+
+
--- tests/rectangle.test.js
+++ tests/rectangle.test.js
@@ -1,4 +1,4 @@
-import {getRectangleArea, getRectanglePerimeter} from "../js/rectangle.js";
+import {getRectangleArea, getRectanglePerimeter, getRectangleInfo} from "../js/rectangle.js";
 
 
 test('should properly output rectangle perimeter', () => {
@@ -9,5 +9,11 @@ test('should properly output rectangle perimeter', () => {
 test('should properly output rectangle area', () => {
     const rectangleArea = getRectangleArea(9, 4);
     expect(rectangleArea).toBe(36);
+});
+
+test('should properly output rectangle info', () => {
+    const logSpy = jest.spyOn(console, 'log');
+    getRectangleInfo(9,4)
+    expect(logSpy).toHaveBeenCalledWith('The perimeter of a rectangle is 26 and the area is 36');
 
 });
